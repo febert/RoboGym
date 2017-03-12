025c65f26e43692f2cf2b5a26a00b25debe0c731
febert <frederik.ebert@mytum.de>
2017-03-12 09:50:41 -0700

    initial commit

---
+++ simulator.py
@@ -0,0 +1,66 @@
+import mujoco_py
+
+
+
+
+class simulator():
+    """
+    Cross Entropy Method Stochastic Optimizer
+    """
+    def __init__(self, params):
+
+
+
+        self.model = mujoco_py.MjModel(self.agentparams['filename'])
+
+        self.verbose = False
+
+        if 'use_first_plan' in self.policyparams:
+            self.use_first_plan = self.policyparams['use_first_plan']
+        else: self.use_first_plan = True
+
+        if 'iterations' in self.policyparams:
+            self.niter = self.policyparams['iterations']
+        else: self.niter = 10  # number of iterations
+
+        self.use_net = self.policyparams['usenet']
+        self.action_list = []
+
+        if self.use_net:
+            hyperparams = imp.load_source('hyperparams', self.policyparams['netconf'])
+            self.netconf = hyperparams.configuration
+
+        self.nactions = self.policyparams['nactions']
+        self.repeat = self.policyparams['repeat']
+        if self.use_net:
+            self.M = self.netconf['batch_size']
+            assert self.nactions * self.repeat == self.netconf['sequence_length']
+            self.predictor = predictor
+            self.K = 10  # only consider K best samples for refitting
+        else:
+            self.M = self.policyparams['num_samples']
+            self.K = 10  # only consider K best samples for refitting
+
+        self.gtruth_images = [np.zeros((self.M, 64, 64, 3)) for _ in range(self.nactions * self.repeat)]
+
+        # the full horizon is actions*repeat
+        # self.action_cost_mult = 0.00005
+        self.action_cost_mult = 0
+
+        self.adim = 2  # action dimension
+        self.initial_std = policyparams['initial_std']
+        if 'exp_factor' in policyparams:
+            self.exp_factor = policyparams['exp_factor']
+
+        gofast = True
+        self.viewer = mujoco_py.MjViewer(visible=True, init_width=480,
+                                         init_height=480, go_fast=gofast)
+        self.viewer.start()
+        self.viewer.set_model(self.model)
+        self.viewer.cam.camid = 0
+
+        self.small_viewer = mujoco_py.MjViewer(visible=True, init_width=64,
+                                         init_height=64, go_fast=gofast)
+        self.small_viewer.start()
+        self.small_viewer.set_model(self.model)
+        self.small_viewer.cam.camid = 0
